f4cac4d4b81c21b8c88dfdbd1c0dd7c6e8a8581f
LouisHM <louismaury19@gmail.com>
2023-11-24 14:25:26 +0100

    alt change

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -27,7 +27,6 @@ export default async function InvoicesTable({
                   <div>
                     <div className="mb-2 flex items-center">
                       <Image
-                        alt={invoice.image_url}
                         src={invoice.image_url}
                         className="mr-2 rounded-full"
                         width={28}
