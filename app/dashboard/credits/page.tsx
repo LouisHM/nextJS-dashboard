195cb82f8c22e6fb531cd10578f0d2215521bb74
LouisHM <louismaury19@gmail.com>
2024-01-12 10:23:59 +0100

    Add image to credits page

--- app/dashboard/credits/page.tsx
+++ app/dashboard/credits/page.tsx
@@ -1,3 +1,14 @@
+import Image from 'next/image';
+
 export default function Page() {
-    return <p>Cet examen à été réalisé par Louis Maury !</p>;
+    return (
+        <p>Cet examen à été réalisé par Louis Maury !</p>
+        <Image
+        src="/hero-desktop.png"
+        width={1000}
+        height={760}
+        className="hidden md:block"
+        alt="Screenshots of the dashboard project showing desktop and mobile versions"
+        />
+    );
 }
