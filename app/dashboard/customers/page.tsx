026bfdc6fce9ba48dc21b5c1eb04b2703f13639e
LouisHM <louismaury19@gmail.com>
2024-01-12 11:21:24 +0100

    Update fetchCustomers function call

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -15,7 +15,7 @@ export default async function Page({
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
 
-  const customersData = await fetchCustomers(query, currentPage);
+  const customersData = await fetchCustomers();
 
   return (
     <div className="w-full">
